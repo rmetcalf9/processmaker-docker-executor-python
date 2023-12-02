f2d1ee68a2e5e7eab2a2c6b7ce4aa178f22b2f45
Robert <rmetcalf9@googlemail.com>
2023-12-02 10:13:49 +0000

    version 1.1.6

--- src/DockerExecutorPythonServiceProvider.php
+++ src/DockerExecutorPythonServiceProvider.php
@@ -13,7 +13,7 @@ class DockerExecutorPythonServiceProvider extends ServiceProvider
 {
     use PluginServiceProviderTrait;
 
-    const version = '1.1.5'; // Required for PluginServiceProviderTrait
+    const version = '1.1.6'; // Required for PluginServiceProviderTrait
 
     public function register()
     {
